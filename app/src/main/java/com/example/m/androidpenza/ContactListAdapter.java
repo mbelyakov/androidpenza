ecc97379806c1d155e881e83485677505429c72c
Mikhail Belyakov <nomail@local.host>
2018-01-25 06:29:17 +0300

    Contact List migrated from RecyclerView to ListFragment

--- app/src/main/java/com/example/m/androidpenza/ContactListAdapter.java
+++ app/src/main/java/com/example/m/androidpenza/ContactListAdapter.java
@@ -1,50 +1,56 @@
 package com.example.m.androidpenza;
 
-import android.support.v7.widget.RecyclerView;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
+public class ContactListAdapter extends ArrayAdapter<Contact> {
+    private final LayoutInflater inflater;
+    private final int layout;
+    private ArrayList<Contact> items;
 
-public class ContactListAdapter extends RecyclerView.Adapter<ContactListAdapter.ViewHolder> {
-    private ArrayList<Contact> contacts;
-
-    public ContactListAdapter(ArrayList<Contact> contacts) {
-        this.contacts = contacts;
+    public ContactListAdapter(@NonNull Context context, int resource, @NonNull ArrayList<Contact> items) {
+        super(context, resource, items);
+        layout = resource;
+        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        this.items = items;
     }
 
+    @NonNull
     @Override
-    public ContactListAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
-        View view = inflater.inflate(R.layout.contact_list_item, parent, false);
-        return new ViewHolder(view);
-    }
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        final ViewHolder holder;
 
-    @Override
-    public void onBindViewHolder(ContactListAdapter.ViewHolder holder, int position) {
-        Contact contact = contacts.get(position);
+        if (convertView == null) {
+            convertView = inflater.inflate(layout, parent, false);
+            holder = new ViewHolder(convertView);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
 
+        Contact contact = items.get(position);
         holder.contactName.setText(contact.name);
         holder.phoneNumber.setText(contact.phoneNumber);
         holder.photo.setImageResource(contact.photo);
+
+        return convertView;
     }
 
-    @Override
-    public int getItemCount() {
-        return contacts.size();
-    }
-
-    class ViewHolder extends RecyclerView.ViewHolder {
-        TextView contactName, phoneNumber;
-        ImageView photo;
+    static class ViewHolder {
+        final TextView contactName;
+        final TextView phoneNumber;
+        final ImageView photo;
 
         ViewHolder(View itemView) {
-            super(itemView);
-
             contactName = itemView.findViewById(R.id.contact_name);
             phoneNumber = itemView.findViewById(R.id.contact_phone_number);
             photo = itemView.findViewById(R.id.contact_photo);
--- app/src/main/java/com/example/m/androidpenza/ContactListFragment.java
+++ app/src/main/java/com/example/m/androidpenza/ContactListFragment.java
@@ -1,50 +1,45 @@
 package com.example.m.androidpenza;
 
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import java.util.ArrayList;
 
+public class ContactListFragment extends ListFragment {
 
-public class ContactListFragment extends Fragment {
-
-    @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        // Возможено ли получить null при вызове getActivity() в данном месте? IDE говорит, что "ДА"
+        Activity activity = getActivity();
+        if (activity != null) {
+            activity.setTitle(R.string.contacts);
+            setListAdapter(new ContactListAdapter(activity, R.layout.contact_list_item, getContacts()));
+        }
+    }
+
+    @NonNull
+    private ArrayList<Contact> getContacts() {
         final int MAX_LIST_SIZE = 20;
         final int PHONE_PREFIX = 8887766;
-
-        super.onCreateView(inflater, container, savedInstanceState);
-
-        View view = inflater.inflate(R.layout.fragment_contact_list, container, false);
-        RecyclerView contactList = view.findViewById(R.id.contacts);
-        // Возможено ли получить NPE при вызове getActivity() в данном месте?
-        contactList.setLayoutManager(new LinearLayoutManager(getActivity()));
 
         ArrayList<Contact> items = new ArrayList<>();
         for (int i = 1; i <= MAX_LIST_SIZE; i++) {
             items.add(new Contact("Имя Фамилия " + i, "+7999" + (PHONE_PREFIX + i), R.mipmap.ic_launcher_round));
         }
-        contactList.setAdapter(new ContactListAdapter(items));
-
-        return view;
+        return items;
     }
 
     @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-
-        Activity activity = getActivity();
-        if (activity != null) {
-            activity.setTitle(R.string.contacts);
-        }
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_contact_list, container, false);
     }
 }
